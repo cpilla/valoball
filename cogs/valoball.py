37524e254b97a0976780091bb8df6f7ab3e72031
tadoyle2024 <89431920+tadoyle2024@users.noreply.github.com>
2023-12-26 23:47:10 -0600

    Duo/Trio Completely Finished

--- cogs/valoball.py
+++ cogs/valoball.py
@@ -135,37 +135,61 @@ class RegistrationMenu(discord.ui.View):
     @discord.ui.button(label="Duo/Trio Queue", style=discord.ButtonStyle.blurple, custom_id="Duo/Trio Queue Button")
     async def duo_trio_queue_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         print("Duo/Trio Queue Button Pressed")
-          
+        
+        numTeams = len(self.bot.queue) // 3
+        numQueues = len(self.bot.duoTrioQueue)
+
         if findDictInList(self.bot.queue, "id", interaction.user.id) == None: # If player is not in queue yet
             await interaction.response.send_message(f"Please join the queue for volleyball first before doing this!", ephemeral=True)
         else:   # If player is in queue
+            if (numTeams <= numQueues):
+                await interaction.response.send_message(f"No more special queues available!", ephemeral=True)
             # Add selector menu for their duo/trio
-
-            await interaction.response.send_message("Please select who you want to queue with!", view=SelectorView(bot=self.bot, interaction=interaction))
+            else:
+                foundFlag = 0
+                for queues in self.bot.duoTrioQueue:
+                    if (findDictInList(queues, "id", interaction.user.id) != None):
+                        foundFlag = 1
+                        break
+                if (foundFlag == 0):
+                    await interaction.response.send_message("Please select who you want to queue with!", view=SelectorView(bot=self.bot, interaction=interaction))
+                else:
+                    await interaction.response.send_message(f"You are already queued with people!", ephemeral=True)
 
         # await interaction.response.defer()
         
         
     @discord.ui.button(label="Exit Queue", style=discord.ButtonStyle.red, custom_id="Exit Queue Button")
     async def exit_queue_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if interaction.user.name not in self.bot.queue:
+        if findDictInList(self.bot.queue, "id", interaction.user.id) == None:
             await interaction.response.send_message(f"You ({interaction.user.name}) are not in the queue for volleyball!", ephemeral=True)
         else:
-            self.bot.queue.remove(interaction.user.name)
+            self.bot.queue.remove(findDictInList(self.bot.queue, "id", interaction.user.id))
+            for queues in self.bot.duoTrioQueue:
+                retVal = findDictInList(queues, "id", interaction.user.id)
+                if (retVal != None):
+                    if (len(queues) == 2):
+                        self.bot.duoTrioQueue.remove(queues)
+                    else:
+                        queues.remove(retVal)
             await interaction.response.send_message(f"You ({interaction.user.name}) have exited the queue for volleyball!", ephemeral=True)
+            print(self.bot.duoTrioQueue)
     
     @discord.ui.button(label="View Queue", style=discord.ButtonStyle.blurple, custom_id="View Queue Button")
     async def view_queue_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         queue_string = ""
-        # leaderboard = json.load(open("leaderboard.json"))
+        leaderboard = json.load(open("leaderboard.json"))
         # print(leaderboard)
         for player in self.bot.queue:
             # print(player)
+            player = findDictInList(leaderboard, "id", player["id"])
             #print(get_rank(self.bot.ranks, player))
             #emoji = "<:" + get_rank(self.bot.ranks, player) + ">"
             queue_string = queue_string + "<:" + get_rank(self.bot.ranks, player) + "> " + "**" + player["name"] + "**  **ELO:** " + str(player["elo"]) + "  **Winrate:**  " + str(round((player["wins"] / max(1, (player["wins"] + player["losses"])) * 100), 2)) + "%\n"
 
         embed = discord.Embed(title="Valoball Queue", description=queue_string,colour=0x00f549)
+        if self.bot.queueMessage != None:
+            await self.bot.queueMessage.delete()
         self.bot.queueMessage = await interaction.channel.send(embed=embed)
         await interaction.response.defer()
     
@@ -388,22 +412,24 @@ class Score_Report(ui.Modal):
     
     async def on_submit(self, interaction: Interaction):
         await interaction.response.defer()
-    
-    
-
-
 
 class Selector(discord.ui.Select):
     def __init__(self, interaction: discord.Interaction, bot):
         self.bot = bot
         selectorOptions = []
         for player in bot.queue:
-            if (player["id"] == interaction.user.id) or (player in bot.duoTrioQueue):
+            if (player["id"] == interaction.user.id):
                 # Don't add if player is the person selecting queues
                 break
             else:
-                playerOption = discord.SelectOption(label = player["name"], value = player["id"], description = player["elo"]) # description= str(player["elo"]))
-            selectorOptions.append(playerOption)
+                foundFlag = 0
+                for queues in bot.duoTrioQueue:
+                    if (findDictInList(queues, "id", player["id"]) != None):
+                        foundFlag = 1
+                        break
+                if (foundFlag == 0):
+                    playerOption = discord.SelectOption(label = player["name"], value = player["id"], description = player["elo"]) # description= str(player["elo"]))
+                    selectorOptions.append(playerOption)
         super().__init__(placeholder="Please choose up to two players to queue with!", min_values=1, max_values=2, options=selectorOptions)
 
     async def callback(self, interaction: discord.Interaction):
@@ -445,7 +471,7 @@ def get_rank(ranks, player):
         ind = ind + 1
 
 def generate_team_name(id):
-    animals = ["Apes","Badgers","Bats","Bears","Buffalo","Cats","Cheetahs","Deer","Dolphins","Donkeys","Echidnas","Elephants","Elk","Ferrets","Foxes","Giraffes","Gorillas","Hares","Hedgehogs","Hippopotamuses","Horses","Hyenas","Jaguars","Kangaroos","Kittens","Lemur","Leopards","Lions","Martens","Mice","Moles","Monkeys","Otters","Platypus","Porcupines","Porpoises","Prairie","dogs","Rabbits","Rhinoceroses","Roes","Rooks","Seals","Sheep","Squirrels","Tigers","Whales","Wolves","Wombats","Zebras"]
+    animals = ["Apes","Badgers","Bats","Bears","Buffalo","Cats","Cheetahs","Deer","Dolphins","Donkeys","Echidnas","Elephants","Elk","Ferrets","Foxes","Giraffes","Gorillas","Hares","Hedgehogs","Hippopotamuses","Horses","Hyenas","Jaguars","Kangaroos","Kittens","Lemur","Leopards","Lions","Martens","Mice","Moles","Monkeys","Otters","Platypus","Porcupines","Porpoises","Prairie","Dogs","Rabbits","Rhinoceroses","Roes","Rooks","Seals","Sheep","Squirrels","Tigers","Whales","Wolves","Wombats","Zebras"]
     adjectives = ["aback","abaft","abandoned","abashed","aberrant","abhorrent","abiding","abject","ablaze","able","abnormal","aboard","aboriginal","abortive","abounding","abrasive","abrupt","absent","absorbed","absorbing","abstracted","absurd","abundant","abusive","acceptable","accessible","accidental","accurate","acid","acidic","acoustic","acrid","actually","ad","hoc","adamant","adaptable","addicted","adhesive","adjoining","adorable","adventurous","afraid","aggressive","agonizing","agreeable","ahead","ajar","alcoholic","alert","alike","alive","alleged","alluring","aloof","amazing","ambiguous","ambitious","amuck","amused","amusing","ancient","angry","animated","annoyed","annoying","anxious","apathetic","aquatic","aromatic","arrogant","ashamed","aspiring","assorted","astonishing","attractive","auspicious","automatic","available","average","awake","aware","awesome","awful","axiomatic","bad","barbarous","bashful","bawdy","beautiful","befitting","belligerent","beneficial","bent","berserk","best","better","bewildered","big","billowy","bite-sized","bitter","bizarre","black","black-and-white","bloody","blue","blue-eyed","blushing","boiling","boorish","bored","boring","bouncy","boundless","brainy","brash","brave","brawny","breakable","breezy","brief","bright","bright","broad","broken","brown","bumpy","burly","bustling","busy","cagey","calculating","callous","calm","capable","capricious","careful","careless","caring","cautious","ceaseless","certain","changeable","charming","cheap","cheerful","chemical","chief","childlike","chilly","chivalrous","chubby","chunky","clammy","classy","clean","clear","clever","cloistered","cloudy","closed","clumsy","cluttered","coherent","cold","colorful","colossal","combative","comfortable","common","complete","complex","concerned","condemned","confused","conscious","cooing","cool","cooperative","coordinated","courageous","cowardly","crabby","craven","crazy","creepy","crooked","crowded","cruel","cuddly","cultured","cumbersome","curious","curly","curved","curvy","cut","cute","cute","cynical","daffy","daily","damaged","damaging","damp","dangerous","dapper","dark","dashing","dazzling","dead","deadpan","deafening","dear","debonair","decisive","decorous","deep","deeply","defeated","defective","defiant","delicate","delicious","delightful","demonic","delirious","dependent","depressed","deranged","descriptive","deserted","detailed","determined","devilish","didactic","different","difficult","diligent","direful","dirty","disagreeable","disastrous","discreet","disgusted","disgusting","disillusioned","dispensable","distinct","disturbed","divergent","dizzy","domineering","doubtful","drab","draconian","dramatic","dreary","drunk","dry","dull","dusty","dynamic","dysfunctional","eager","early","earsplitting","earthy","easy","eatable","economic","educated","efficacious","efficient","eight","elastic","elated","elderly","electric","elegant","elfin","elite","embarrassed","eminent","empty","enchanted","enchanting","encouraging","endurable","energetic","enormous","entertaining","enthusiastic","envious","equable","equal","erect","erratic","ethereal","evanescent","evasive","even excellent excited","exciting exclusive","exotic","expensive","extra-large extra-small exuberant","exultant","fabulous","faded","faint fair","faithful","fallacious","false familiar famous","fanatical","fancy","fantastic","far"," far-flung"," fascinated","fast","fat faulty","fearful fearless","feeble feigned","female fertile","festive","few fierce","filthy","fine","finicky","first"," five"," fixed"," flagrant","flaky","flashy","flat","flawless","flimsy"," flippant","flowery","fluffy","fluttering"," foamy","foolish","foregoing","forgetful","fortunate","four frail","fragile","frantic","free"," freezing"," frequent"," fresh"," fretful","friendly","frightened frightening full fumbling functional","funny","furry furtive","future futuristic","fuzzy ","gabby","gainful","gamy","gaping","garrulous","gaudy","general gentle","giant","giddy","gifted","gigantic","glamorous","gleaming","glib","glistening glorious","glossy","godly","good","goofy","gorgeous","graceful","grandiose","grateful gratis","gray greasy great","greedy","green grey grieving","groovy","grotesque","grouchy","grubby gruesome","grumpy","guarded","guiltless","gullible gusty","guttural H habitual","half","hallowed","halting","handsome","handsomely","handy","hanging","hapless","happy","hard","hard-to-find","harmonious","harsh","hateful","heady","healthy","heartbreaking","heavenly heavy hellish","helpful","helpless","hesitant","hideous high","highfalutin","high-pitched","hilarious","hissing","historical","holistic","hollow","homeless","homely","honorable","horrible","hospitable","hot huge","hulking","humdrum","humorous","hungry","hurried","hurt","hushed","husky","hypnotic","hysterical","icky","icy","idiotic","ignorant","ill","illegal","ill-fated","ill-informed","illustrious","imaginary","immense","imminent","impartial","imperfect","impolite","important","imported","impossible","incandescent","incompetent","inconclusive","industrious","incredible","inexpensive","infamous","innate","innocent","inquisitive","insidious","instinctive","intelligent","interesting","internal","invincible","irate","irritating","itchy","jaded","jagged","jazzy","jealous","jittery","jobless","jolly","joyous","judicious","juicy","jumbled","jumpy","juvenile","kaput","keen","kind","kindhearted","kindly","knotty","knowing","knowledgeable","known","labored","lackadaisical","lacking","lame","lamentable","languid","large","last","late","laughable","lavish","lazy","lean","learned","left","legal","lethal","level","lewd","light","like","likeable","limping","literate","little","lively","lively","living","lonely","long","longing","long-term","loose","lopsided","loud","loutish","lovely","loving","low","lowly","lucky","ludicrous","lumpy","lush","luxuriant","lying","lyrical","macabre","macho","maddening","madly","magenta","magical","magnificent","majestic","makeshift","male","malicious","mammoth","maniacal","many","marked","massive","married","marvelous","material","materialistic","mature","mean","measly","meaty","medical","meek","mellow","melodic","melted","merciful","mere","messy","mighty","military","milky","mindless","miniature","minor","miscreant","misty","mixed","moaning","modern","moldy","momentous","motionless","mountainous","muddled","mundane","murky","mushy","mute","mysterious","naive","nappy","narrow","nasty","natural","naughty","nauseating","near","neat","nebulous","necessary","needless","needy","neighborly","nervous","new","next","nice","nifty","nimble","nine","nippy","noiseless","noisy","nonchalant","nondescript","nonstop","normal","nostalgic","nosy","noxious","null","numberless","numerous","nutritious","nutty","oafish","obedient","obeisant","obese","obnoxious","obscene","obsequious","observant","obsolete","obtainable","oceanic","odd","offbeat","old","old-fashioned","omniscient","one","onerous","open","opposite","optimal","orange","ordinary","organic","ossified","outgoing","outrageous","outstanding","oval","overconfident","overjoyed","overrated","overt","overwrought","painful","painstaking","pale","paltry","panicky","panoramic","parallel","parched","parsimonious","past","pastoral","pathetic","peaceful","penitent","perfect","periodic","permissible","perpetual","petite","petite","phobic","physical","picayune","pink","piquant","placid","plain","plant","plastic","plausible","pleasant","plucky","pointless","poised","polite","political","poor","possessive","possible","powerful","precious","premium","present","pretty","previous","pricey","prickly","private","probable","productive","profuse","protective","proud","psychedelic","psychotic","public","puffy","pumped","puny","purple","purring","pushy","puzzled","puzzling","quack","quaint","quarrelsome","questionable","quick","quickest","quiet","quirky","quixotic","quizzical","rabid","racial","ragged","rainy","rambunctious","rampant","rapid","rare","raspy","ratty","ready","real","rebel","receptive","recondite","red","redundant","reflective","regular","relieved","remarkable","reminiscent","repulsive","resolute","resonant","responsible","rhetorical","rich","right","righteous","rightful","rigid","ripe","ritzy","roasted","robust","romantic","roomy","rotten","rough","round","royal","ruddy","rude","rural","rustic","ruthless","sable","sad","safe","salty","same","sassy","satisfying","savory","scandalous","scarce","scared","scary","scattered","scientific","scintillating","scrawny","screeching","second","second-hand","secret","secretive","sedate","seemly","selective","selfish","separate","serious","shaggy","shaky","shallow","sharp","shiny","shivering","shocking","short","shrill","shut","shy","sick","silent","silent","silky","silly","simple","simplistic","sincere","six","skillful","skinny","sleepy","slim","slimy","slippery","sloppy","slow","small","smart","smelly","smiling","smoggy","smooth","sneaky","snobbish","snotty","soft","soggy","solid","somber","sophisticated","sordid","sore","sore","sour","sparkling","special","spectacular","spicy","spiffy","spiky","spiritual","spiteful","splendid","spooky","spotless","spotted","spotty","spurious","squalid","square","squealing","squeamish","staking","stale","standing","statuesque","steadfast","steady","steep","stereotyped","sticky","stiff","stimulating","stingy","stormy","straight","strange","striped","strong","stupendous","stupid","sturdy","subdued","subsequent","substantial","successful","succinct","sudden","sulky","super","superb","superficial","supreme","swanky","sweet","sweltering","swift","symptomatic","synonymous","taboo","tacit","tacky","talented","tall","tame","tan","tangible","tangy","tart","tasteful","tasteless","tasty","tawdry","tearful","tedious","teeny","teeny-tiny","telling","temporary","ten","tender tense","tense","tenuous","terrible","terrific","tested","testy","thankful","therapeutic","thick","thin","thinkable","third","thirsty","thoughtful","thoughtless","threatening","three","thundering","tidy","tight","tightfisted","tiny","tired","tiresome","toothsome","torpid","tough","towering","tranquil","trashy","tremendous","tricky","trite","troubled","truculent","true","truthful","two","typical","ubiquitous","ugliest","ugly","ultra","unable","unaccountable","unadvised","unarmed","unbecoming","unbiased","uncovered","understood","undesirable","unequal","unequaled","uneven","unhealthy","uninterested","unique","unkempt","unknown","unnatural","unruly","unsightly","unsuitable","untidy","unused","unusual","unwieldy","unwritten","upbeat","uppity","upset","uptight","used","useful","useless","utopian","utter","uttermost","vacuous","vagabond","vague","valuable","various","vast","vengeful","venomous","verdant","versed","victorious","vigorous","violent","violet","vivacious","voiceless","volatile","voracious","vulgar","wacky","waggish","waiting","","wakeful","wandering","wanting","warlike","warm","wary","wasteful","watery","weak","wealthy","weary","well-groomed","well-made","well-off","well-to-do","wet","whimsical","whispering","white","whole","wholesale","wicked","wide","wide-eyed","wiggly","wild","willing","windy","wiry","wise","wistful","witty","woebegone","womanly","wonderful","wooden","woozy","workable","worried","worthless","wrathful","wretched","wrong","wry","xenophobic","yellow","yielding","young","youthful","yummy","zany","zealous","zesty","zippy","zonked"]
     adjectives = [adjective.capitalize() for adjective in adjectives]
     last4 = id[len(id)- 4:]
